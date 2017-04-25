35ab38ce3ee4c56f47a5fee72d030b6877b0fcbf
salahusama <salaheldin.akl@gmail.com>
2017-04-25 23:11:40 +0100

    added activity to view calendar

--- TimeStamper/app/src/main/java/oop/project/timestamper/Home.java
+++ TimeStamper/app/src/main/java/oop/project/timestamper/Home.java
@@ -1,7 +1,11 @@
 package oop.project.timestamper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    // called when view_button pressed
+    public void viewCalendar(View view) {
+        Intent intent = new Intent(this, CalendarActivity.class);
+        startActivity(intent);
+    }
+
+    // called when stamp_button pressed
+    public void stamp(View view) {
+        Intent intent = new Intent(this, StampActivity.class);
+        startActivity(intent);
     }
 }
